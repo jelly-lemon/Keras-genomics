ba740da5761927b43a5765bfa8fd62601ecd3285
b312-lemon <lemeng1@qq.com>
2021-09-24 15:43:40 +0800

    测试模型 Model1

--- models/Model1.py
+++ models/Model1.py
@@ -18,9 +18,9 @@ class Model1(BaseModel):
         super().__init__(save_dir)
 
     def train(self, x_train, y_train, x_val, y_val):
-        checkpoint = ModelCheckpoint(filepath=self.weight_path, verbose=1, save_best_only=True)
+        # checkpoint = ModelCheckpoint(filepath=self.weight_path, verbose=1, save_best_only=True)
         # EarlyStopping(monitor='val_loss', patience=3, verbose=0)
-        callbacks = [checkpoint]
+        callbacks = []
 
         train_params = {
             "batch_size": 32,
@@ -52,13 +52,15 @@ class Model1(BaseModel):
         # 构建模型
         #
         model = Sequential()
-        model.add(Conv2D(16, (1, 24), padding='same', input_shape=model_params["input_shape"],
+        model.add(Conv2D(16, (3, 3), padding='same', input_shape=model_params["input_shape"],
                          activation='relu'))  # input_shape 不包含 batch_size
+        model.add(BatchNormalization())
+        model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+        model.add(BatchNormalization())
         model.add(GlobalMaxPooling2D())
         model.add(Dense(32, activation='relu'))
-        model.add(BatchNormalization())
-        model.add(Dense(model_params["class_num"]))
-        model.add(Activation(activation=model_params["activation"]))
+        model.add(Dense(model_params["class_num"], activation=model_params["activation"]))
+        model.summary()
 
         #
         # 超参数
